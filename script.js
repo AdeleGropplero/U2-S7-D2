6bad070b49562c6eb2f5b446c0bab23c27b471f6
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-12 16:23:40 +0100

    jbj

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
 const form = document.getElementById("form");
+const saveBtn = document.getElementById("save");
 /* console.log(form); */
 
-form.onsubmit = function (e) {
-  e.preventDefault();
+const generateAlert = function () {
+  const alertDiv = document.getElementById("alert");
 
-  const input =
-    document.getElementById(
-      "inputName"
-    ).value; /* perchè con form non lo prende? */
-  /*  console.log(input); */
+  const alert = document.createElement("div");
+  alert.className = "alert alert-danger";
+  alert.role = "alert";
+  alert.innerText = "Nessun elemento precedente trovato";
 
-  localStorage.setItem("input-inserito", input);
-
-  form.reset();
-
-  const h1 = document.querySelector("h1");
-  h1.innerText = `Ciao ${input}!`;
+  alertDiv.appendChild(alert);
 };
 
-const inputFromStorage = localStorage.getItem("input-inserito");
+const generateAlert2 = function () {
+  const alertDiv = document.getElementById("alert");
+
+  const alert = document.createElement("div");
+  alert.className = "alert alert-info";
+  alert.role = "alert";
+  alert.innerText = "Elemento rimosso con successo";
+
+  alertDiv.appendChild(alert);
+};
+
+const h1 = document.querySelector("h1");
+const defaultMessage = h1.innerText;
+
+const removeName = function () {
+  const inputFromStorage = localStorage.getItem("input-inserito");
+  if (inputFromStorage) {
+    localStorage.removeItem("input-inserito");
+
+    h1.innerText = defaultMessage;
+    console.log("sono entrato nell if");
+    generateAlert2();
+  } else {
+    generateAlert();
+    console.log(" NOON sono entrato nell if");
+  }
+};
+
+const clearBtn = document.getElementById("clear");
+
+window.addEventListener("DOMContentLoaded", function () {
+  form.onsubmit = function (e) {
+    e.preventDefault();
+
+    const input =
+      document.getElementById(
+        "inputName"
+      ).value; /* perchè con form non lo prende? */
+    /*  console.log(input); */
+
+    localStorage.setItem("input-inserito", input);
+
+    form.reset();
+
+    const h1 = document.querySelector("h1");
+    h1.innerText = `Ciao ${input}!`;
+    console.log(h1.innerText);
+
+    clearBtn.onclick = function (e) {
+      e.preventDefault();
+
+      removeName();
+    };
+  };
+  const inputFromStorage = localStorage.getItem("input-inserito");
+  if (inputFromStorage) {
+    const h1 = document.querySelector("h1");
+    h1.innerText = `Ciao ${inputFromStorage}!`;
+  }
+});
